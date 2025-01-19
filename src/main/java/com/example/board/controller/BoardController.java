8fd1d09f80ec654ef5e79cbce3dba8036a44b2fd
Jin <jinnkimm7@gmail.com>
2025-01-19 19:55:01 +0900

    게시글 작성 데이터 전송하기

--- src/main/java/com/example/board/controller/BoardController.java
+++ src/main/java/com/example/board/controller/BoardController.java
@@ -1,12 +1,26 @@
 package com.example.board.controller;
 
+import com.example.board.dto.BoardDto;
 import com.example.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class BoardController {
     private final BoardService boardService;
 
+    @GetMapping("/save")
+    public String save() {
+        return "save";
+    }
+
+    @PostMapping("/save")
+    public String save(BoardDto boardDto) {
+        System.out.println("BoardController.save");
+        System.out.println("boardDto = " + boardDto);
+        return "index";
+    }
 }
